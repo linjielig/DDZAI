7d8bc66d7ea6f3b10992624e43d01ebfb24aae6f
jie <linjielig@icloud.com>
2019-03-20 16:46:04 +0800

    优化注释

--- DDZAI/Architecture/AI__.cs
+++ DDZAI/Architecture/AI__.cs
@@ -1,18 +1,17 @@
 ﻿/*
      * 需求：   
-     * 1. 根据三家牌的数据，准备判断是否叫地主，加倍。
+     * 1. 根据三家牌的数据，准确判断是否叫地主，加倍。
      * 2. 根据三家牌的数据，自己的位置，自己是否是地主，准确判断是否出牌，出什么牌。
-     * 3. 根据需求调整程序判断的准确度，以赢钱或输钱。
+     * 3. 根据需求调整程序判断的准确度。
      * 
      * 架构：
-     * 1. 评分组件，根据三家牌的数据，准确的对三家手中牌，手中牌加上底牌或模拟出牌之后的牌进行评分，
-     * 评分越低的牌获胜几率越大，三家评分的关系决定是否叫地主，是否加倍。
+     * 1. 评分组件，根据三家牌的数据，准确的对三家手中牌，手中牌加上底牌或模拟出牌或过牌之后的牌进行评分，
+     * 评分越低的牌获胜几率越大。发牌后，三家评分的关系决定是否叫地主，是否加倍。
      * 2. 出牌组件，根据三家评分的关系，自己的位置，自己是否是地主，来判断是否出牌，出什么牌。
      * 3. 环境组件，提供谁是地主，三个玩家的牌数据，三个玩家的位置关系。
      * 4. 智力组件，调整提供数据的多少和判断的准确度。
      */
 
-using System;
 using System.Collections.Generic;
 
 namespace DDZ {
@@ -20,7 +19,6 @@ namespace DDZ {
         my,
         other1,
         other2,
-        other12
     }
 
     public interface IAI__ {
@@ -43,14 +41,14 @@ namespace DDZ {
         bool IsLandOrDouble(LimitType type) {
             // 有其他玩家牌的数据。
             if (environment.IsHaveOtherData()) {
-                // 自己的牌比另外两家好的程度。
+                // 自己的牌与其他玩家中最好的牌比较，符合限制。
                 if (score.My() - score.BetterOther() < score.Limit(type)) {
                     return true;
                 }
             }
             // 没有其他玩家牌的数据
-            // 自己的牌好的程度。
             else {
+                // 自己的牌符合限制。
                 // 此处IScore.Limit返回的限制与上方的不同，表示的是没有其他玩家数据时的限制。
                 if (score.My() < score.Limit(type)) {
                     return true;
@@ -73,19 +71,19 @@ namespace DDZ {
         public List<byte> GetOut() {
             // 是否打别人的牌。
             if (environment.GetLastOut() != null) {
-                    // 模拟过牌。
-                    out_.SimulatePass();
-                    int passRanking = score.SimulateRanking();
-                    // 模拟出牌。
-                    out_.Simulate();
-                    int outRanking = score.SimulateRanking();
-                    // 出牌后我评分的排名可能下降，不出牌。
-                    // 如果我是牌比较差的农民，这里返回的是牌比较好的农民评分的排名，
-                    // 出牌后牌比较好的农名评分的排名可能下降，不出牌。
-                    if (passRanking > outRanking) {
-                        return null;
-                    }
+                // 模拟过牌。
+                out_.SimulatePass();
+                int passRanking = score.SimulateRanking();
+                // 模拟出牌。
+                out_.Simulate();
+                int outRanking = score.SimulateRanking();
+                // 出牌后我评分的排名可能下降，不出牌。
+                // 如果我是牌比较差的农民，这里返回的是牌比较好的农民评分的排名，
+                // 出牌后牌比较好的农民评分的排名可能下降，不出牌。
+                if (passRanking > outRanking) {
+                    return null;
                 }
+            }
 
             return out_.Get();
         }
--- DDZAI/Architecture/Out__.cs
+++ DDZAI/Architecture/Out__.cs
@@ -5,7 +5,10 @@ namespace DDZ {
     // 出牌组件。
     interface IOut__ {
         // 获取要打出的牌。
-        List<byte> Get(bool isSimulation = false);
+        List<byte> Get();
+        /* 模拟仅计算一轮之后的情况，即自己出牌或者过牌之后，再次轮到自己出牌，此时三家牌的评分关系。
+         * 此为可能的情况，不是100%的准确。       
+         */
         // 模拟出牌。
         void Simulate();
         // 模拟不出牌。
